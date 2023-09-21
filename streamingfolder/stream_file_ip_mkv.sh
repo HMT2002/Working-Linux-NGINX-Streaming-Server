0abd77065ee65b41ed76538025dcfc5f26418581
HMT2002 <bluesea070802@gmail.com>
2023-09-21 22:05:43 +0700

    fix linux command

--- streamingfolder/stream_file_ip_mkv.sh
+++ streamingfolder/stream_file_ip_mkv.sh
@@ -1,2 +1,1 @@
-chcp 65001
-ffmpeg -stream_loop -1 -y -re -i "$1.mkv" -vcodec copy -loop -1 -c:a aac -b:a 160k -ar 44100 -strict -2 -f flv "rtmp:$2/live/$1"
+ffmpeg -stream_loop -1 -y -re -i "$1.mkv" -vcodec copy -loop -1 -c:a aac -b:a 160k -ar 44100 -strict -2 -f flv "rtmp:$2/live/$1_mkv"
